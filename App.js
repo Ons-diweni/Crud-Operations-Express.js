d53c490e2ad9ee55efcce677936c2e0bd87a76e2
Ons <ons.diweni@esprit.tn>
2023-03-07 22:39:11 +0100

    database connection

--- App.js
+++ App.js
@@ -2,11 +2,22 @@ require('dotenv').config();
 
 //import express module
 const express = require('express')
+//import mongoose module 
+const mongoose = require ('mongoose')
 
+//database connection 
+mongoose.connect(process.env.DB_URL , {useNewUrlParser: true, useUnifiedTopology: true})
+.then(()=> console.log('Connexion à MongoDB réussie !'))
+.catch((erreur)=>console.log('Connexion à MongoDB échouée !' + erreur))
 
 // create a new instance of the Express application 
 const App = express ()
 
+//express.json() is a middleware function that extracts the JSON data from the request body and parses it into a JavaScript object, 
+//which can then be accessed in your application code. 
+//This middleware function is typically used for APIs that receive JSON data in the request body like this one
+App.use(express.json)
+App.use(express.urlencoded({ extended: false }));
 
 
 
--- server.js
+++ server.js
@@ -3,9 +3,36 @@ require('dotenv').config()
 
 //import http module
 const http = require('http')
+const App = require('./App')
 
+const errorHandler = error => {
+    if (error.syscall !== 'listen') {
+        throw error;
+    }
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port: ' + port;
+    switch (error.code) {
+        case 'EACCES':
+            console.error(bind + ' requires elevated privileges.');
+            process.exit(1);
+            break;
+        case 'EADDRINUSE':
+            console.error(bind + ' is already in use.');
+            process.exit(1);
+            break;
+        default:
+            throw error;
+    }
+};
 
 
 //make server instance
-const server = http.createServer()
-server.listen(process.env.PORT , ()=> console.log('server runnig with port '+process.env.PORT))
+const server = http.createServer(App)
+server.on('error', errorHandler);
+server.on('listening', () => {
+    const address = server.address();
+    const bind = typeof address === 'string' ? 'pipe ' + address : 'port ' + process.env.PORT;
+    console.log('Listening on ' + bind);
+});
+server.on('request' , (req,res) => {console.log(req.url)})
+server.listen(process.env.PORT)
