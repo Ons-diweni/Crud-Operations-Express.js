69d82deed69abb087c922c5fbeba0fff0e9cdcae
Ons <ons.diweni@esprit.tn>
2023-03-08 17:25:10 +0100

    updating add (getAll - getById) EndPoint

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -2,7 +2,7 @@ const express = require ('express')
 const User = require ('../models/User.js')
 
 
-//*****************************Create ****************/
+//***************************** Endpoint to Create a user ****************/
 
  exports.add = (req , res , next ) => {
     console.log(req.body)
@@ -17,15 +17,17 @@ const User = require ('../models/User.js')
     
   }
 
-//***************************** Read ******************/
-exports.getAll = (req , res , next ) => {
+//***************************** Enpoint to  (get All users - get user By Id )  ******************/
 
-    User.find()
-    .then(users =>res.status(201).json(users))
-    .catch(err => res.status(400).json({err}));
-
-  }
-
+exports.find = (req , res , next ) => {
+ 
+    const id = req.params.id ;
+    (id)? User.findOne({_id :req.params.id })
+    .then((data) => {(data)? res.send(data):res.status(404).send({message :"Not found user with id "+ req.params.id })})
+    .catch((err) =>res.status(500).send({ message: "Error retrieving user with id " + req.params.id})): User.find()
+    .then((users) => res.send(users))
+    .catch((err)=> res.send({message : "Error retrieving users"}))
+} 
 //*************************getById ********************/
 
 exports.getById = ( req , res , next ) => {
@@ -45,15 +47,6 @@ User.findOne({_id : req.params.id})
 
 /********************************************** */
 
-exports.find = (req , res , next ) => {
- 
-    const id = req.params.id ;
-    (id)? User.findOne({_id :req.params.id })
-    .then((data) => {(data)? res.send(data):res.status(404).send({message :"Not found user with id "+ req.params.id })})
-    .catch((err) =>res.status(500).send({ message: "Error retrieving user with id " + req.params.id})): User.find()
-    .then((users) => res.send(users))
-    .catch((err)=> res.send({message : "Error retrieving users"}))
-} 
 
 
 
